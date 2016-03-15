d13138acc54412feb37347c31a6c55002c6f10ed
Christopher Chan <chanculture@gmail.com>
2016-03-15 22:28:32 +0800

    Add initial schema design

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,25 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+DROP DATABASE IF EXISTS tournament;
 
+CREATE DATABASE tournament;
+
+\c tournament;
+
+CREATE TABLE IF NOT EXISTS players (
+    id			serial PRIMARY KEY,
+    name		varchar(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS matches (
+    winner    		integer references players(id),
+    loser    		integer references players(id)
+);
+
+CREATE VIEW standings AS
+	SELECT p.id id, p.name player,
+	(SELECT count(*) FROM matches m WHERE p.id = m.winner) wins,
+	(SELECT count(*) FROM matches m WHERE p.id = m.winner OR p.id = m.loser) matches
+	FROM players p
+	ORDER BY wins DESC;
