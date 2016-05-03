8857c4ac15cdf3f39ed7af55a4279535de17b242
Christopher Chan <chanculture@gmail.com>
2016-05-03 22:25:04 +0800

    Allow uploaded image files to have upper case extensions

--- vagrant/catalog/project.py
+++ vagrant/catalog/project.py
@@ -52,7 +52,7 @@ session = DBSession()
 # For a given file, return whether it's an allowed type or not
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
 
 
 def uploadItemImage(file, item):
